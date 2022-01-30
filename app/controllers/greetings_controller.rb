1db61f8ce059fa64971c61b62a6d9c066dbf242c
Ahmet Bozaci <ahmt9417@gmail.com>
2022-01-30 19:32:03 +0300

    Fix rubocop errors

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,13 +1,11 @@
-
-  class GreetingsController < ApplicationController
-    def index
-      @greetings = Greeting.all
-      render json: @greetings
-    end
-
-    def show
-      @greeting = Greeting.find(params[:id])
-      render json: @greeting
-    end
+class GreetingsController < ApplicationController
+  def index
+    @greetings = Greeting.all
+    render json: @greetings
   end
 
+  def show
+    @greeting = Greeting.find(params[:id])
+    render json: @greeting
+  end
+end
