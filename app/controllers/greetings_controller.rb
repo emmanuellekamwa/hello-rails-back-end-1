6eb0a920878d05fc0de82d0ffd3a167aaf3daff1
Ahmet Bozaci <ahmt9417@gmail.com>
2022-01-30 16:43:07 +0300

    Update route

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,4 +1,13 @@
-class GreetingsController < ApplicationController
-  def index
+
+  class GreetingsController < ApplicationController
+    def index
+      @greetings = Greeting.all
+      render json: @greetings
+    end
+
+    def show
+      @greeting = Greeting.find(params[:id])
+      render json: @greeting
+    end
   end
-end
+
